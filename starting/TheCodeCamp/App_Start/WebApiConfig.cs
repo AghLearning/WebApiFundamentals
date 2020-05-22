70af2ec9643afa8855080cafc41bc31e26d95e31
adolfo.garcia <adolfo.garcia@rdtingenieros.com>
2020-05-22 09:29:54 +0200

    Curso Completado

--- starting/TheCodeCamp/App_Start/WebApiConfig.cs
+++ starting/TheCodeCamp/App_Start/WebApiConfig.cs
@@ -1,7 +1,15 @@
-﻿using System;
+﻿using Microsoft.Web.Http;
+using Microsoft.Web.Http.Routing;
+using Microsoft.Web.Http.Versioning;
+using Microsoft.Web.Http.Versioning.Conventions;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Routing;
+using TheCodeCamp.Controllers;
 
 namespace TheCodeCamp
 {
@@ -9,17 +17,49 @@ namespace TheCodeCamp
   {
     public static void Register(HttpConfiguration config)
     {
-      // Web API configuration and services
-      AutofacConfig.Register();
+        // Web API configuration and services
+        AutofacConfig.Register();
 
-      // Web API routes
-      config.MapHttpAttributeRoutes();
+        config.AddApiVersioning(cfg =>
+        {
+            cfg.DefaultApiVersion = new ApiVersion(1, 1);                       // Version por defecto
+            cfg.AssumeDefaultVersionWhenUnspecified = true;
+            cfg.ReportApiVersions = true;
+            //cfg.ApiVersionReader = new HeaderApiVersionReader("X-Version");   // configuración para recuperar la versión del Header
+            //cfg.ApiVersionReader = ApiVersionReader.Combine(                    // Definir multiples gestiones de la version.
+            //        new HeaderApiVersionReader("X-version"),
+            //        new QueryStringApiVersionReader("ver")
+            //    );
+            cfg.ApiVersionReader = new UrlSegmentApiVersionReader();            // Lee la version de la ruta.
 
-      config.Routes.MapHttpRoute(
-          name: "DefaultApi",
-          routeTemplate: "api/{controller}/{id}",
-          defaults: new { id = RouteParameter.Optional }
-      );
-    }
+            cfg.Conventions.Controller<TalksController>()
+                .HasApiVersion(1, 0)
+                .HasApiVersion(1, 1)
+                .Action(m => m.Get(default(string), default(int), default(bool)))
+                    .MapToApiVersion(2, 0);
+
+
+        });
+
+
+        //Modificar el formato de las variables JSON para que sean CamelCase
+        config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            var constraintResolver = new DefaultInlineConstraintResolver()
+            {
+                ConstraintMap =
+                {
+                    ["apiVersion"]  = typeof(ApiVersionRouteConstraint)
+                }
+            };
+
+        // Web API routes
+        config.MapHttpAttributeRoutes(constraintResolver);
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //);
+        }
   }
 }
---
+++ starting/TheCodeCamp/Models/CampModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TheCodeCamp.Models
+{
+    public class CampModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Moniker { get; set; }
+        [Required]
+        public DateTime EventDate { get; set; } = DateTime.MinValue;
+        [Required]
+        [Range(1,30)]
+        public int Length { get; set; } = 1;
+
+        /* 
+         * Talks: Es una propiedad IEnumerable en la Entidad.
+         */
+         public ICollection<TalkModel> Talks { get; set; }
+
+        /* 
+         *  Como la relacion con la localización es 1:1 podemos incluirla dentro del mismo modelo. 
+         *  Si añadimos al campo el nombre de la entidad como prefijo, automapper lo reconoce y lo mapea.
+         */
+        public string Venue { get; set; }
+        public string LocationAddress1 { get; set; }
+        public string LocationAddress2 { get; set; }
+        public string LocationAddress3 { get; set; }
+        public string LocationCityTown { get; set; }
+        public string LocationStateProvince { get; set; }
+        public string LocationPostalCode { get; set; }
+        public string LocationCountry { get; set; }
+
+
+    }
+}
